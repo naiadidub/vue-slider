3887d09e8282c4452484b2afe69d8318bbbef79a
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-21 17:10:46 +0200

    stop active

---
+++ script.js
@@ -0,0 +1,59 @@
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            
+            slides : [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ],
+            currentIndex : 0,
+            thumbIndex : 0,
+        },
+         
+        
+        methods: {
+            previous(){
+                if(this.currentIndex === 0 && this.thumbIndex === 0){
+                    this.currentIndex = 4
+                    this.thumbIndex = 4
+                } else {
+                    this.currentIndex --
+                    this.thumbIndex --
+                }
+            },
+            prox(){
+                if(this.currentIndex === 4 && this.thumbIndex === 4){
+                    this.currentIndex = 0
+                    this.thumbIndex = 0
+                } else {
+                    this.currentIndex ++
+                    this.thumbIndex ++
+                }
+            }
+        }
+    }
+)
